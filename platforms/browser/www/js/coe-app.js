bf924230696297b65207cedc32ef8993f6bae130
kophonegap <niconi@coca-cola.com>
2016-05-07 02:54:13 +0800

    new

---
+++ platforms/browser/www/js/coe-app.js
@@ -0,0 +1,16 @@
+
+	$(".detailbox").addClass("hidden");
+			
+	$("#gg1_details").on("click",function(){
+		//alert("Hello");
+		// var slider1 = $("#swiper1");
+		// var details1 = $("#gg1left");
+        	/*if(slider1.hasClass("swiper-slide-active")){
+            $(this).details1(".detailbox").show();
+        }else {
+			 $(".detailbox").hide();
+		}*/
+		$("#gg1left").removeClass("hidden");
+            
+        return false;
+    });
